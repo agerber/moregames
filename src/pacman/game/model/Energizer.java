ea723a87ed3c99bfd5e64965aa7c73771f0f4cad
Adam Gerber <adam.gerber@yahoo.com>
2020-11-26 18:07:03 -0600

    add pacman; remove sounds

---
+++ src/pacman/game/model/Energizer.java
@@ -0,0 +1,51 @@
+package pacman.game.model;
+
+import pacman.controller.Game;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * Created by pmnehls on 11/19/14.
+ */
+public class Energizer extends Sprite
+{
+    private int nEnergizerX;
+    private int nEnergizerY;
+    public Energizer(int x, int y)
+    {
+        super();
+
+        nEnergizerX = x;
+        nEnergizerY = y;
+
+        ArrayList<Point> pntCs = new ArrayList<Point>();
+
+        pntCs.add(new Point(2, 4));
+        pntCs.add(new Point(4, 2));
+        pntCs.add(new Point(4, -2));
+        pntCs.add(new Point(2, -4));
+        pntCs.add(new Point(-2, -4));
+        pntCs.add(new Point(-4, -2));
+        pntCs.add(new Point(-4, 2));
+        pntCs.add(new Point(-2, 4));
+
+        assignPolarPoints(pntCs);
+
+        setColor(new Color(200,200,200));
+
+        int nHeight = TargetSpace.TS_HEIGHT;
+        int nWidth = TargetSpace.TS_WIDTH;
+
+        setCenter(new Point(nHeight * (x - 1) + nHeight/2, nWidth * (y - 1) + nWidth/2));
+
+        setRadius(8);
+
+    }
+
+    public void draw(Graphics g) {
+        super.draw(g);
+        g.fillPolygon(getXcoords(), getYcoords(), dDegrees.length);
+    }
+
+}
