09d6fc771e4e4dd699bffbd4f813996c1e9f6f82
Adam Gerber <adam.gerber@yahoo.com>
2021-09-23 00:07:04 -0500

    refactor packages for sinistar

---
+++ src/main/java/edu/uchicago/gerber/sinistar/mvc/model/DestroyableFoe.java
@@ -0,0 +1,20 @@
+package edu.uchicago.gerber.sinistar.mvc.model;
+
+/**
+ * Created by michaelvasiliou on 11/19/16.
+ */
+public class DestroyableFoe extends Sprite{
+    private int health;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean mineCrystal(){
+        return false;
+    };
+}
