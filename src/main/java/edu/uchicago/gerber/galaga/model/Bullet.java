3c16bffb714c71560ee5865dd99d9765312d7cca
Adam Gerber <adam.gerber@yahoo.com>
2022-11-15 09:44:47 -0600

    add partial galaga mod

---
+++ src/main/java/edu/uchicago/gerber/galaga/model/Bullet.java
@@ -0,0 +1,50 @@
+package edu.uchicago.gerber.galaga.model;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bullet extends Sprite {
+
+
+
+    public Bullet(Falcon fal) {
+
+
+
+        setTeam(Team.FRIEND);
+
+        //a bullet expires after 20 frames. set to one more than frame expiration
+        setExpiry(21);
+        setRadius(6);
+
+
+        //everything is relative to the falcon ship that fired the bullet
+        setCenter(fal.getCenter());
+
+        //set the bullet orientation to the falcon (ship) orientation
+        setOrientation(fal.getOrientation());
+
+        final double FIRE_POWER = 35.0;
+        setDeltaX(fal.getDeltaX() +
+                Math.cos(Math.toRadians(fal.getOrientation())) * FIRE_POWER);
+        setDeltaY(fal.getDeltaY() +
+                Math.sin(Math.toRadians(fal.getOrientation())) * FIRE_POWER);
+
+
+        //defined the points on a cartesian grid
+        List<Point> pntCs = new ArrayList<>();
+        pntCs.add(new Point(0, 3)); //top point
+        pntCs.add(new Point(1, -1));
+        pntCs.add(new Point(0, -2));
+        pntCs.add(new Point(-1, -1));
+
+        setCartesians(pntCs);
+
+
+
+    }
+
+
+
+}
